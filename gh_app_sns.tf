dd4c7572f2fcbfa65b2f7cb19e1943c9a3ef77bd
Kalyan Vedala <svedala23@gmail.com>
2025-01-06 08:11:11 -0800

    Add IAM policy to allow sns access from ecs

--- gh_app_sns.tf
+++ gh_app_sns.tf
@@ -7,3 +7,26 @@ resource "aws_sns_topic_subscription" "pipelineci_sns_runner_subscription" {
   protocol  = "https"
   endpoint  = "https://${var.RUNNER_SUBDOMAIN}.${var.DOMAIN_NAME}/run_ci"
 }
+
+resource "aws_iam_policy" "pipelineci_sns_policy" {
+  name = "pipelineci_sns_policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "sns:Publish",
+          "sns:ListTopics"
+        ],
+        Resource: aws_sns_topic.pipelineci_sns_runner_topic.arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "sns_role_attachment" {
+  policy_arn  = aws_iam_policy.pipelineci_sns_policy.arn
+  role        = aws_iam_role.ecs_execution_role.name
+}
